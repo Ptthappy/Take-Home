7b28ec4d6b6a2bcaef32ca97805c2fa06786e02e
Luis Petrella <aax_00ap@hotmail.com>
2023-09-18 13:48:43 -0400

    adjusting tests and adding root endpoint for verify if it's working

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,5 +1,11 @@
 import { FastifyPluginAsync } from 'fastify'
 
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {}
+const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+    fastify.get('/', async function (request, reply) {
+        reply.send({
+            data: 'api working!'
+        })
+    })
+}
 
 export default root;
